747f767238d68a2011d2033a230cd8964294f217
AlexAlvarezAlmendros <alex.capellades16@gmail.com>
2025-08-01 11:23:15 +0200

    Refactor useEvents hook to include sorting by date in descending order

--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -3,7 +3,10 @@ import { Cards } from '../components/CardList/CardList'
 import CreateButton from '../components/CreateButton/CreateButton'
 
 function Eventos () {
-  const { events, loading, error } = useEvents()
+  const { events, loading, error } = useEvents({
+    sortBy: 'date',
+    sortOrder: 'desc'
+  })
   return (
     <>
       <h1>Últimos Eventos</h1>
